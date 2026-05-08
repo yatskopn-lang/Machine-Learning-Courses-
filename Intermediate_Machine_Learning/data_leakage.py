7eb84ef055ed92f38b1f60ea36300b00ecdbd42e
alinaytsk <alinayatsko10@gmail.com>
2026-05-08 18:53:45 +0300

    Solved code tasks from Data Leakage

---
+++ Intermediate_Machine_Learning/data_leakage.py
@@ -0,0 +1,11 @@
+"""Data Leakage"""
+
+#Setup
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_intermediate.ex7 import *
+print("Setup Complete")
+
+#Step 5: Housing Prices
+potential_leakage_feature = 2
+
